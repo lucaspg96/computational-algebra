a08583c50ef729dc07796fc2c3566bc2041db740
Lucas Peres <lucaspg96@live.com>
2018-04-18 21:08:44 -0300

    Starting Displaced Power Method

---
+++ src/main/scala/eigenvaluesCalculators/PowerMethodDisplaced.scala
@@ -0,0 +1,9 @@
+package eigenvaluesCalculators
+
+import entities.{Matrix, Vector}
+
+class PowerMethodDisplaced {
+  def calculate(A: Matrix, tolerance: Double = 0.001): (List[Double], List[Vector]) = {
+    
+  }
+}
